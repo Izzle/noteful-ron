f46f6700e0d908bf93c55474a9b8a6155c69baec
Ron Martin <ronmartin89@gmail.com>
2020-02-05 23:31:55 -0800

    baseline for React Router in place

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+import dummyStore from './dummy-store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+
+  state = {};
+
+  componentDidMount() {
+    // Ajax stuff
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
